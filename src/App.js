3f7339878843f81a592f36f80644965774b2eb09
Rafael Furry <rfurry@gmail.com>
2018-04-16 10:05:11 -0700

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import 'styling/semantic.less';
 import { Navbar, Home } from 'components';
 
 const App = () => (
   <div>
     <Navbar />
-    <Home />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
   </div>
 );
 
