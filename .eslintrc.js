d51b9dfb0110e7358c34d789e75c04919039273f
Rafael Furry <rfurry@gmail.com>
2018-04-16 10:39:33 -0700

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    "extends": "airbnb",
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaVersion": 6
-    },
-    "rules": {
-        "react/jsx-filename-extension": 0,
-        "import/extensions": "off",
-        "import/no-unresolved": "off"
-    },
-    "env": {
-        "browser": true,
-        "es6": true
-    }
+  extends: "airbnb",
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 6
+  },
+  rules: {
+    "react/jsx-filename-extension": 0,
+    "import/extensions": "off",
+    "import/no-unresolved": "off"
+  },
+  env: {
+    browser: true,
+    es6: true
+  }
 };
